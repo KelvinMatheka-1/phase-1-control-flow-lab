dec8316adb394c523f0059d9157bb0bb1ea28164
Kelvin M Matheka <beingmatheka@gmail.com>
2022-08-16 09:50:41 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 function scuberGreetingForFeet(someNumber) {
 
   if (someNumber <= 400){
-     return "This one is on me!";
+    return "This one is on me!";
   }
   else if (someNumber > 2000 && someNumber < 2500){
-    return "I will gladly take your thirty bucks.";
+      return "I will gladly take your thirty bucks.";
   }
   else if(someNumber > 2500){
-    return "No can do.";
+      return "No can do.";
   }
 
 }
+
 function ternaryCheckCity(city) {
 
-  if (city === "NYC") {
-      return "Ok, sounds good.";
-  }
-  else{
-      return "No go.";
-  }
+  return(city === 'NYC')? "Ok, sounds good." : "No go."
 }
+
 
 function switchOnCharmFromTip(tip) {
 
@@ -30,4 +27,6 @@ function switchOnCharmFromTip(tip) {
   } else {
       return "Bye.";
   }
+
+
 }
